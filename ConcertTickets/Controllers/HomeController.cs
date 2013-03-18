74b689014cb0a12e832b9cd32d571c1a22ca428f
Erik Konecny <konecny94264@gotoltc.edu>
2013-03-17 20:34:17 -0500

    Initial build of concert tickets web app

---
+++ ConcertTickets/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConcertTickets.Models;
+
+
+namespace ConcertTickets.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult OrderForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult OrderForm(CustomerResponse customerResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", customerResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ConcertTickets/Models/CustomerResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcertTickets.Models
+{
+    public class CustomerResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your mailing address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Please select a show")]
+        public string Show { get; set; }
+
+        [Required(ErrorMessage = "Please enter a quantity")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a positive number")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage="Please specify how you would like to receive your tickets")]
+        public bool? MailTickets { get; set; }
+    }
+}
